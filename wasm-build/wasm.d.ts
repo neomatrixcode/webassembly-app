a73692c6e0df611c3bc5ec674e808433a76e4cd3
josue acevedo maldonado <josuecevedo@gmail.com>
2021-05-21 22:03:23 -0500

    wasm-build

---
+++ wasm-build/wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @returns {number}
+*/
+export function net(): number;
+/**
+* @param {number} a
+* @param {number} b
+* @returns {number}
+*/
+export function add_two_ints(a: number, b: number): number;
+/**
+* @param {number} n
+* @returns {number}
+*/
+export function fib(n: number): number;
---
+++ wasm-build/wasm_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function net(): number;
+export function add_two_ints(a: number, b: number): number;
+export function fib(a: number): number;
